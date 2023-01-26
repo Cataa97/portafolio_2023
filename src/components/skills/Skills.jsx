d7f3df71136107f0b16a91568f0ce2fea64679bf
Catalina <catalina.castillo19c@gmail.com>
2023-01-26 16:17:48 -0300

    iconscout

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,11 @@
+import {Fade} from "react-awesome-reveal";
+import React from 'react'
+import './Skills.css';
+
+export const Skills = () => {
+    return (
+        <div className='skills-main-div'>
+            <div className='skills-image-div'></div>
+        </div>
+    )
+}
